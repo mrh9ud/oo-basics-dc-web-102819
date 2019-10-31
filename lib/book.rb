610f83fb6205943ddf28817efffcf9a61011d55c
mrh9ud <mrh9ud@virginia.edu>
2019-10-30 22:17:52 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,38 @@
+class Book
+    def initialize(title)
+        @title = title
+    end
 
+    def title
+        @title
+    end
+
+    def author=(author_name)
+        @author = author_name
+    end
+
+    def author
+        @author
+    end
+
+    def page_count=(page_num)
+        @page_count = page_num
+    end
+
+    def page_count
+        @page_count
+    end
+
+    def genre=(genre)
+        @genre = genre
+    end
+
+    def genre
+        @genre
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
